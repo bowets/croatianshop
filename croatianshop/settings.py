a5edebb356a21893f3c823805a332084dae849c8
bforsek <bforsek@gmail.com>
2021-07-19 22:43:27 +0100

    Add allauth package and update urls for account

--- croatianshop/settings.py
+++ croatianshop/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    # User applications
 ]
 
 MIDDLEWARE = [
@@ -66,6 +71,13 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
 
 WSGI_APPLICATION = 'croatianshop.wsgi.application'
 
