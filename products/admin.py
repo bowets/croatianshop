62bb2c8f91b73626e3bc45025ae2f2d2de763a44
bforsek <bforsek@gmail.com>
2021-07-20 13:32:53 +0100

    Add custom admin view of products

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
 from .models import Product, Category, Manufacturer
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Manufacturer)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = (
+    'name',
+    'friendly_name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Manufacturer, ManufacturerAdmin)
