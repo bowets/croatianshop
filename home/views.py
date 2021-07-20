71537a01ec6ef48e812c3f2c7f757415da1ca25d
bforsek <bforsek@gmail.com>
2021-07-20 14:47:48 +0100

    Add products to the product page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from products.models import Product
 
 def index(request):
     """Renders the home page template"""
-    return render(request, 'home/index.html')
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'home/index.html', context)
