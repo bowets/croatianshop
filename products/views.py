4e763658baf762b1e995b95d3606a1eea549f6b5
bforsek <bforsek@gmail.com>
2021-07-23 17:09:04 +0100

    Add travis.yml to start CI testing with Travis

--- products/views.py
+++ products/views.py
@@ -43,8 +43,7 @@ def all_products(request):
                 sortkey == "lower_name"
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == "manufacturer":
-                sortkey = "lower_manufacturer"
-                products = products.annotate(lower_manufacturer=Lower('manufacturer'))
+                sortkey = "manufacturer__name"
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == "desc":
