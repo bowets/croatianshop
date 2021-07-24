241be5ee35430ac8b387de4e5e55c1252968f384
bforsek <bforsek@gmail.com>
2021-07-24 23:32:58 +0100

    Add and debug stripe card element

--- purchase/static/purchase/js/stripe_elements.js
+++ purchase/static/purchase/js/stripe_elements.js
@@ -1,31 +1,28 @@
-let stripe_public_key = $('#id_stripe_public_key').text().slice(1,-1);
-let client_secret = $('#id_client_secret').text().slice(1,-1);
-console.log(stripe_public_key);
-let stripe = Stripe(stripe_public_key);
-let elements = stripe.elements();
-console.log(elements);
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
 
-let card = elements.create('card', {
-    style: {
-      base: {
-        iconColor: '#c4f0ff',
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var style = {
+    base: {
         color: '#000',
-        fontWeight: '500',
-        fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
-        fontSize: '16px',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
         fontSmoothing: 'antialiased',
-        ':-webkit-autofill': {
-          color: '#fce883',
-        },
+        fontSize: '16px',
         '::placeholder': {
-          color: '#87BBFD',
-        },
-      },
-      invalid: {
-        iconColor: '#DC3545',
-        color: '#DC3545',
-      },
+            color: '#aab7c4'
+        }
     },
-  });
-
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', {style: style});
 card.mount('#card-element');
