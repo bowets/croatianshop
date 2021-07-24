976d17f941d54d04fb5e753abf9625662b43bffd
bforsek <bforsek@gmail.com>
2021-07-25 00:09:11 +0100

    Add payment intent to purchase view

--- purchase/views.py
+++ purchase/views.py
@@ -1,3 +1,4 @@
+from croatianshop.settings import STRIPE_CURRENCY
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
@@ -10,6 +11,9 @@ import stripe
 
 
 def purchase(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There is nothing in your cart at the moment")
@@ -18,12 +22,23 @@ def purchase(request):
     current_cart = cart_contents(request)
     total = current_cart['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency = settings.STRIPE_CURRENCY
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'purchase/purchase.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51J8TH6JCvONQIszN8CmraPKb2feb0d8dbpiyyQVp04gdk4JAfOOobiXanmWd8o09EJV12hocqJbYhyJuzC43d3LC00VARaiq8D',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
