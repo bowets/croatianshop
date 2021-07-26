f713313631187aecb94bfb14f98adfad109aee30
bforsek <bforsek@gmail.com>
2021-07-26 23:07:10 +0100

    Add functionality to save user profile data from delivery information to user profile

--- purchase/views.py
+++ purchase/views.py
@@ -1,3 +1,4 @@
+from profiles.forms import UserProfileForm
 from django.http.response import HttpResponse
 from django.views.decorators.http import require_POST
 from croatianshop.settings import STRIPE_CURRENCY
@@ -9,6 +10,8 @@ from checkout.contexts import cart_contents
 from django.conf import settings
 from .models import OrderLineItem, Order
 from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 
 import stripe
 import json
@@ -73,7 +76,7 @@ def purchase(request):
                     order.delete()
                     return redirect(reverse('view_bag'))
 
-            request.session['save_info'] = 'save_info' in request.POST
+            request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('purchase_success', args=[order.order_number]))
         else:
             messages.error(request, "There was an error with your form. \
@@ -93,7 +96,25 @@ def purchase(request):
             currency = settings.STRIPE_CURRENCY
         )
 
-        order_form = OrderForm()
+        
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'eircode': profile.default_eircode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -114,6 +135,29 @@ def purchase_success(request, order_number):
 
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+    
+    
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_eircode': order.eircode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
             email will be sent to {order.email}.')
