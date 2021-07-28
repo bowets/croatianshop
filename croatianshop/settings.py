07d3dd82564edba32f64fffc039654e7975f0c04
bforsek <bforsek@gmail.com>
2021-07-28 21:14:32 +0100

    Update allowed hosts

--- croatianshop/settings.py
+++ croatianshop/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bowets-croatian-food-shop.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['bowets-croatian-food-store.herokuapp.com', 'localhost']
 
 
 # Application definition
