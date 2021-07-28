d4f6fba6b09b9603186909f1c3352b3c2025e1cd
bforsek <bforsek@gmail.com>
2021-07-29 00:14:16 +0100

    Remove duplicate DEFAULT_FROM_EMAIL

--- croatianshop/settings.py
+++ croatianshop/settings.py
@@ -214,7 +214,6 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
-DEFAULT_FROM_EMAIL = 'info@croatianfoodshop.com'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
