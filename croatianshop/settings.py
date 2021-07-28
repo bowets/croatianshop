e2f64f7e72a5d93cb13626e11f3767c6f0e11afc
bforsek <bforsek@gmail.com>
2021-07-29 00:07:23 +0100

    Revert email settings back to gmail

--- croatianshop/settings.py
+++ croatianshop/settings.py
@@ -222,10 +222,10 @@ if 'DEVELOPMENT' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_PORT = 465
-    EMAIL_HOST = 'smtp.sendgrid.net'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER_SENDGRID')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS_SENDGRID')
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
