12d43041cd7e0ec48f05a00b699e3cc230b53a67
bforsek <bforsek@gmail.com>
2021-07-30 21:40:54 +0100

    Update test.py in the home app

--- home/tests.py
+++ home/tests.py
@@ -1,11 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse, resolve
-from .views import index
+
+
 
 # Create your tests here.
 
-class TestURLs(TestCase):
-
-    def test_home_url_is_resolved(self):
-        url = reverse('home')
-        self.assertEquals(resolve(url).func, index)
